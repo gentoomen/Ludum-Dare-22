cbf663da606773c35458da53488c456bfe56df85
Chown <sunderphon+git@gmail.com>
2011-12-18 08:18:49 +0000

    Working map parser

--- resources/maps/readmap.py
+++ resources/maps/readmap.py
@@ -3,6 +3,7 @@ import json
 class obj():
 	def __init__(self, objfile=None):
 		self.objfile = objfile
+		#other shit can go here too, this is just to test
 
 class GameMap():
 	def parseFile(self, addr):
@@ -15,13 +16,15 @@ class GameMap():
 		self.terrain = content['map']['terrain']
 		self.layout = content['map']['layout']
 
-		self.schema = []
+		self.schema = {}
 		for i in content['map']['aliases']:
 			f = open(i,'r')
 			str = f.read()
 			f.close()
-			self.schema.append(json.JSONDecoder().decode(str))
+			self.schema[i] = json.JSONDecoder().decode(str)
 
 		self.objlist = []
-		for k in content['map']['layout'].keys():
-			self.objlist.append(obj(self.schema['objects'][k]))
+
+		for alias in content['map']['aliases']:
+			for mapobj in content['map']['layout']:
+				self.objlist.append(obj(self.schema[alias]['objects'][mapobj]))
